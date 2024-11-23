9c77a8a1b53992a376d797db63b0d431075117c4
FireFlyDeveloper <saludeskimdev@gmail.com>
2024-11-23 10:54:15 +0800

    first commit

---
+++ Activity.java
@@ -0,0 +1,19 @@
+public class Activity {
+    public static void main(String[] args) {
+        Student student = new Student("Kim Eduard Saludes", "Balayan", "Information Technology", 2);
+
+        System.out.println("Student Details:");
+        System.out.println("Name: " + student.getName());
+        System.out.println("Address: " + student.getAddress());
+        System.out.println("Program: " + student.getProgram());
+        System.out.println("Year: " + student.getYear());
+
+        Staff staff = new Staff("Bobby Diosanta", "Dilaw", "Computer Engineering Technology", 10.0);
+
+        System.out.println("\nStaff Details:");
+        System.out.println("Name: " + staff.getName());
+        System.out.println("Address: " + staff.getAddress());
+        System.out.println("School: " + staff.getSchool());
+        System.out.println("Salary: " + staff.getSalary());
+    }
+}
